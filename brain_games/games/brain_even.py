d02b90175f28bd93da034ff3aac8e128e99a8278
Rustam <isabaevrustam@gmail.com>
2021-04-08 01:17:59 +0500

    STEP 7

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -1,14 +1,15 @@
 from random import randint
+from brain_games import cli
 import prompt
 
 
-def check(name):
+def check_even(name):
     n = 0
     while n < 3:
-        random_number = randint(1, 100)
-        print('Question: {}'.format(random_number))
+        r_num = randint(1, 100)
+        print('Question: {}'.format(r_num))
         answer = prompt.string('Your answer: ')
-        if (random_number % 2) == 0:
+        if (r_num % 2) == 0:
             if answer == 'yes':
                 n += 1
                 print('Correct!')
@@ -18,7 +19,7 @@ def check(name):
                     " Correct answer was 'yes'.\nLet's try again, {}!"
                     .format(answer, name))
                 break
-        elif (random_number % 2) != 0:
+        elif (r_num % 2) != 0:
             if answer == 'no':
                 n += 1
                 print('Correct!')
@@ -32,10 +33,10 @@ def check(name):
 
 
 def brain_even():
-    name = prompt.string('May i have you name? ')
+    name = cli.welcome_user()
     print(
-        'Hello, {}!\nAnswer "yes" if the number is even, otherwise answer "no".'
-        .format(name))
-    n = check(name)
+        'Answer "yes" if the number is even, otherwise answer "no".'
+    )
+    n = check_even(name)
     if n == 3:
         print('Congratulations, {}!'.format(name))
