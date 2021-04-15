9e1e5cfd773df36a8ac61026c0a749e85d5657d2
Rustam <isabaevrustam@gmail.com>
2021-04-15 16:28:41 +0500

    Fix #2

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,8 +1,12 @@
 import prompt
 
 
-def run(text):
+def welcome_user():
     print("Welcome to Brain Games!")
+
+
+def run(text):
+    welcome_user()
     name = prompt.string('May i have you name? ')
     print("Hello, {}!".format(name))
     print(text)
